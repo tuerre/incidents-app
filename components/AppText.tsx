19ac5cf0d6a3b1128345a0b8593798de017988e4
tuerre <jendryjhancel@gmail.com>
2026-02-01 12:46:59 -0400

    feat: add GuestHome, IncidenciasScreen, SplashScreen, AppText component, and Supabase service

---
+++ components/AppText.tsx
@@ -0,0 +1,13 @@
+import { Text, TextProps } from 'react-native'
+
+export function AppText(props: TextProps) {
+    return (
+        <Text
+            {...props}
+            style={[
+                { fontFamily: 'PoppinsRegular' },
+                props.style,
+            ]}
+        />
+    )
+}
